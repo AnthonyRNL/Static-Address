7812a91960784c8426899f169c4dcdaf2f882dd6
Matt Fewer <mattfewer@hotmail.com>
2015-08-29 17:34:36 -0400

    resized the soundcloud player on the show page

--- static_address/app/assets/javascripts/soundcloudstuff.js
+++ static_address/app/assets/javascripts/soundcloudstuff.js
@@ -7,7 +7,7 @@ function playSomeSound(genre) {
 		}
 	}, function(tracks) {
 		var random = Math.floor(Math.random() * 49);
-		SC.oEmbed(tracks[random].uri, {autho_play: true}, document.getElementById('target'));
+		SC.oEmbed(tracks[random].uri, {auto_play: true, maxheight: 200}, document.getElementById('target'));
 	})
 }
 
