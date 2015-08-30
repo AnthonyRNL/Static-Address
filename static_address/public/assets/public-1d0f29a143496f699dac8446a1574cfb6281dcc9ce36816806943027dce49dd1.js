c66b9b132a155a9f99dda5c81b38a4ce43c43445
Matt Fewer <mattfewer@hotmail.com>
2015-08-30 14:43:05 -0400

    index is working, show page is working, life is good

--- static_address/public/assets/public-1d0f29a143496f699dac8446a1574cfb6281dcc9ce36816806943027dce49dd1.js
+++ static_address/public/assets/public-1d0f29a143496f699dac8446a1574cfb6281dcc9ce36816806943027dce49dd1.js
@@ -16,6 +16,7 @@ window.onload = function(){
 		client_id: '8a1760755d85f0e6579508e64c401791'
 	})
 	
+	var url = params[]
 	SC.oEmbed('https://soundcloud.com/underoath/writing-on-the-walls', {auto_play: true, maxheight: 200, iframe: true, color: 'FFFFFF'}, document.getElementById('target'));
 	
 	$('target').scPlayer();
@@ -31,3 +32,17 @@ window.onload = function(){
 //		}
 //	}
 }
+;
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
