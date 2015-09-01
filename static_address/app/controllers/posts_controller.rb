6762aa7da0fb3d0b179fa8f65327c1cdc6120a01
Matt Fewer <mattfewer@hotmail.com>
2015-09-01 15:28:04 -0400

    Show page and Posts page look much nicer now

--- static_address/app/controllers/posts_controller.rb
+++ static_address/app/controllers/posts_controller.rb
@@ -4,18 +4,17 @@ class PostsController < ApplicationController
 #	before_action :logged_in_user, only: [:create]
 	#before_action :authenticate
 	
-	def index
-		
+	def index	
 		if params[:user_id]
-			@posts = Post.where({user_id: params[:user_id]})
+			@posts = Post.where({user_id: params[:user_id], avatar: params[:user_id]})
 		else
 			@posts = Post.all
 		end
-
 	end
 	
 	def show
 		@post = Post.find(params[:id])
+		@user = User.find(params[:id])
 	end
 	
 	def create
