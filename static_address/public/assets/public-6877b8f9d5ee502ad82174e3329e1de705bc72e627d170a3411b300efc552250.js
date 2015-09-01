be2c8e78ea8ab370a94f15c52d40dc8faa465569
Matt Fewer <mattfewer@hotmail.com>
2015-09-01 09:22:03 -0400

    got rid of the youtube links, the show page works from index

--- static_address/public/assets/public-6877b8f9d5ee502ad82174e3329e1de705bc72e627d170a3411b300efc552250.js
+++ static_address/public/assets/public-6877b8f9d5ee502ad82174e3329e1de705bc72e627d170a3411b300efc552250.js
@@ -1,3 +1,24 @@
+SC.initialize({
+	client_id: '8a1760755d85f0e6579508e64c401791'
+	
+	window.onload = function () {
+
+	var urlWanted = url || 'https://soundcloud.com/underoath/writing-on-the-walls';
+	SC.oEmbed(urlWanted, {auto_play: true, maxheight: 200, iframe: true}, document.getElementById('target'));
+
+$('target').scPlayer(function(url){
+	url.reload()
+})
+}
+})
+
+
+
+
+
+
+$(document).ready(initialize);
+$(document).on('page:load', initialize);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -10,11 +31,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require foundation
-//= require soundcloudstuff
-//= require_tree .
-$(function() {
-  $(document).foundation();
-});
+
