ecf0aff392023fc5706f88cbb62f44b382c4cc78
Loaner 2 <null>
2015-09-01 09:27:20 -0400

    need to do this to pull

--- static_address/app/assets/javascripts/soundcloudstuff.js
+++ static_address/app/assets/javascripts/soundcloudstuff.js
@@ -1,12 +1,10 @@
 SC.initialize({
-		client_id: '8a1760755d85f0e6579508e64c401791'
-	})
+        client_id: '8a1760755d85f0e6579508e64c401791'
+    })
 
 window.onload = function(){
-	var urlWanted = url || 'https://soundcloud.com/underoath/writing-on-the-walls';
-	SC.oEmbed(urlWanted, {auto_play: true, maxheight: 200, iframe: true, color: 'FFFFFF'}, document.getElementById('target'));
-}
+    var urlWanted = url || 'https://soundcloud.com/underoath/writing-on-the-walls';
+    SC.oEmbed(urlWanted, {auto_play: true, maxheight: 200, iframe: true, color: 'FFFFFF'}, document.getElementById('target'));
 
-window.setTimeout(function(){
-	$('target').scPlayer();
-}, 2000)
+    $('target').scPlayer()
+ }
