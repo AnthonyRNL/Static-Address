5346dc011705efc7270dab8cb09a1bc83c87d64c
Matt Fewer <mattfewer@hotmail.com>
2015-09-01 20:45:08 -0400

    soundcloud and youtube are FINALLY working. had to update criteria in the DB, add soundcloudfile & youtubefile and migrated, also added the sckck

--- static_address/app/assets/javascripts/soundcloudstuff.js
+++ static_address/app/assets/javascripts/soundcloudstuff.js
@@ -1,27 +1,28 @@
-$(document).ready(initialize);
-$(document).on('page:load', initialize);
-
+$(document).ready(initialize());
 
 function initialize() {
 	SC.initialize({
 		client_id: '8a1760755d85f0e6579508e64c401791'
 	})
 
+	window.onload = function () {
 
-
-		var urlWanted = url || 'https://soundcloud.com/underoath/writing-on-the-walls';
-		SC.oEmbed(urlWanted, {
-			auto_play: false,
-			maxheight: 200,
-			iframe: true
-		}, document.getElementById('target'));
-
-		$('target').scPlayer(function (url) {
-			url.reload()
-		})
-	
+//				window.setTimeout(function(){
+//					$('target').scPlayer();
+//				}, 2000)
+				
+		if (mediaNum === "1") {
+			$target = document.getElementById('target')
+			var urlWanted = url || 'https://soundcloud.com/underoath/writing-on-the-walls';
+			console.log(url)
+			SC.oEmbed(urlWanted, {auto_play: false, maxheight: 200, iframe: true}, document.getElementById('target'));
+			
+		} else if (mediaNum === "2") {
+			var urlWanted = url || "www.youtu.be/bg1sT4ILG0w"
+			$target = document.getElementById('target')
+			embedurl = url.toString().replace('watch?v=', 'embed/')
+			$('<iframe id="media-player" height="200" src="' + embedurl + '" frameborder="0" allowfullscreen></iframe>').appendTo(target)
+			console.log($target)
+		}
+	}
 }
-
-
-
-
--- static_address/db/seeds.rb
+++ static_address/db/seeds.rb
@@ -6,15 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: "Matthew", username: "MasterMatthew", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
-User.create(name: "Anthony", username: "MasterAnthony", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
-User.create(name: "Raina", 	 username: "MasterRaina", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
-User.create(name: "Steven",  username: "MasterSteven", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
+User.create(id: 1, name: "Matthew", password_digest: "yo", username: "MasterMatthew", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
+User.create(id: 2, name: "Anthony", password_digest: "yo", username: "MasterAnthony", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
+User.create(id: 3, name: "Raina", password_digest: "yo", username: "MasterRaina", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
+User.create(id: 4, name: "Steven", password_digest: "yo", username: "MasterSteven", email: "What@What.com", avatar: "http://cdn.1xrun.com/images/uploads/3_immer.jpg")
 
-Post.create(title: "The Kinky Fuzz EP", user_id: 1, content: "This is Matt Fewer here. Seeding some new data for my new website. Checkout this original instrumental I recorded with the shake",   upvote: 0, soundfile: "https://soundcloud.com/matt-fewer/kinky-fuzz/s-ZdAab")
-Post.create(title: "Das mah shiit", user_id: 1, content: "Just can't get this song out of my head!! Needed to post it! Sorry I know this forum is for original music only but had to share haha :)", upvote: 0, soundfile: "https://soundcloud.com/riserecords/sets/silverstein-i-am-alive-in")
-Post.create(title: "New Track: Already Gone", user_id: 1, content: "Here's another fresh new track off my CD, The Kinky Fuzz Ep", upvote: 0, soundfile: "https://soundcloud.com/matt-fewer/already-gone/s-IbaZb")
-Post.create(title: "One More: Paralyzed", user_id: 1, content: "OK OK, I'm going a little crazy with the posts. But I had to just show you guys the last but not least song on the album.",  upvote: 0, soundfile: "https://soundcloud.com/matt-fewer/paralyzed/s-JcQ05")
+Post.create(title: "The Kinky Fuzz EP", user_id: 1, content: "This is Matt Fewer here. Seeding some new data for my new website. Checkout this original instrumental I recorded with the shake",   upvote: 2, soundfile: "https://soundcloud.com/matt-fewer/kinky-fuzz/s-ZdAab")
+Post.create(title: "Das mah shiit", user_id: 1, content: "Just can't get this song out of my head!! Needed to post it! Sorry I know this forum is for original music only but had to share haha :)", upvote: 3, soundfile: "https://soundcloud.com/riserecords/sets/silverstein-i-am-alive-in")
+Post.create(title: "New Track: Already Gone", user_id: 1, content: "Here's another fresh new track off my CD, The Kinky Fuzz Ep", upvote: 10, soundfile: "https://soundcloud.com/matt-fewer/already-gone/s-IbaZb")
+Post.create(title: "One More: Paralyzed", user_id: 1, content: "OK OK, I'm going a little crazy with the posts. But I had to just show you guys the last but not least song on the album.",  upvote: 900, soundfile: "https://soundcloud.com/matt-fewer/paralyzed/s-JcQ05")
 
 Comment.create(post_id: 4, user_id: 3, content: "NOT Cool stuff")
 Comment.create(post_id: 3, user_id: 2, content: "I HATE THIS SONG")
